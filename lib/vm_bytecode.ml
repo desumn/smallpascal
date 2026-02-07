b1a17d797ce00fa1fcc25f2e5405db9a98cf5a8c
desumn <desumn@posteo.net>
2026-02-07 09:27:20 +0100

    Change bytcode

--- lib/vm_bytecode.ml
+++ lib/vm_bytecode.ml
@@ -1,14 +1,21 @@
 
 
-type no_change = [ `Nop | `Exit ]
-and adder = [ `Push of int | `LoadLocal of int ]
-and remover = [ `Pop | `StoreLocal of int ]
-and unary_operator = [ `Dup | `Not ]
-and binary_operator =
-  [ `Add | `Sub | `Mul | `Or | `And | `Swap |
+type zero_to_zero = [`Nop | `Exit]
+and zero_to_one = [`Push of int]
+and one_to_zero = [`Pop]
+and one_to_one = [`Not]
+and two_to_two = [`Swap]
+and one_to_two = [`Dup] 
+and two_to_one =
+  [ `Add | `Sub | `Mul | `Or | `And |
     `Equal | `NotEqual | `Greater | `Lesser |
     `GreaterEqual | `LesserEqual ]
+and locals = [`StoreLocal of int | `LoadLocal of int]
 and control = [`Jump of int | `JumpIfZero of int]
 
 
-type instruction = [ no_change | adder | remover | unary_operator | binary_operator | control ]
+type instruction = [ zero_to_zero | zero_to_one | one_to_zero | one_to_one | two_to_two |
+                     one_to_two | two_to_one | locals | control]
+
+type unary_operator = [one_to_one | one_to_two]
+type binary_operator = [two_to_one | two_to_two]
