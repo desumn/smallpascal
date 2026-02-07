fdfb4a0a4148528d65f0cc3ede225df0fdc46796
desumn <desumn@posteo.net>
2026-02-07 09:52:18 +0100

    Add an operator type

--- lib/vm_bytecode.ml
+++ lib/vm_bytecode.ml
@@ -1,8 +1,6 @@
 
 
 type zero_to_zero = [`Nop | `Exit]
-and zero_to_one = [`Push of int]
-and one_to_zero = [`Pop]
 and one_to_one = [`Not]
 and two_to_two = [`Swap]
 and one_to_two = [`Dup] 
@@ -10,12 +8,14 @@ and two_to_one =
   [ `Add | `Sub | `Mul | `Or | `And |
     `Equal | `NotEqual | `Greater | `Lesser |
     `GreaterEqual | `LesserEqual ]
+and basic_manipulation = [`Push of int | `Pop]
 and locals = [`StoreLocal of int | `LoadLocal of int]
 and control = [`Jump of int | `JumpIfZero of int]
 
 
-type instruction = [ zero_to_zero | zero_to_one | one_to_zero | one_to_one | two_to_two |
-                     one_to_two | two_to_one | locals | control]
+type instruction = [ zero_to_zero | one_to_one | two_to_two |
+                     one_to_two | two_to_one | basic_manipulation | locals | control]
 
 type unary_operator = [one_to_one | one_to_two]
 type binary_operator = [two_to_one | two_to_two]
+type operator = [unary_operator | binary_operator]
